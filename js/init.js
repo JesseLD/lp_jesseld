98a3b7226f82625c3e571d70551d700e36c54802
Jessé Oliveira <82299456+JesseLD@users.noreply.github.com>
2024-10-16 17:48:57 -0300

    Updated 1.1

---
+++ js/init.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded",(e)=>{
+
+  document.querySelector("#copy").innerText = `© ${new Date().getFullYear()}  Jessé Oliveira. Todos os direitos reservados.`
+});
